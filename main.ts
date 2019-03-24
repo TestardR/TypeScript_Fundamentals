c125f5ac59da0d358a74e324093944c62f86608d
unknown <rmtestard@gmail.com>
2019-03-24 15:55:16 +0100

    interfaces

--- main.ts
+++ main.ts
@@ -1,7 +1,33 @@
 // compiling and variables
-const bob = 'hey I am bob';
-const bob2 = () => {};
-console.log(bob);
+
+const num = 5;
+
+const dec = 5.232;
+
+const obj = {
+  x: 5,
+  y: 9.4
+};
+
+// explicit definitions
+const a: number = 5.23;
+const obj1: { x: number; y: number } = {
+  x: 22,
+  y: 3.2
+};
+
+// using interface
+
+interface obj2 {
+  x: number;
+  y: number;
+}
+
+const obj2: obj2 = {
+  x: 22,
+  y: 33
+};
+
 // functions
 
 // unions
